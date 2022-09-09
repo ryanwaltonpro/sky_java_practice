c65a5f937be4071a4a7fcb0b2d797478d3d2439c
Ryan Walton <ryanwalton1999@gmail.com>
2022-09-09 16:40:14 +0100

    Added employee

---
+++ Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+    private String name;
+
+    public Employee(String name){
+        name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- sayHello.java
+++ sayHello.java
@@ -1,6 +1,7 @@
 public class sayHello {
     public static void main(String[] args)
     {
+        Employee employee = new Employee("Ryan");
         System.out.println(args[0]);
     }
 }
