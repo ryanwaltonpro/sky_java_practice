ea6761d2dbaeb68f9cee588d395acedfff0f6bd4
Ryan Walton <ryanwalton1999@gmail.com>
2022-09-12 15:39:19 +0100

    conditionals 1

---
+++ Conditionals.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Conditionals {
+    public static void main(String[] args){
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter a number: ");
+        int num2 = scanner.nextInt();
+        System.out.println((num1 > num2 ? "num 1" : "num 2") + " is bigger");
+
+        System.out.println("Enter a number: ");
+        int num3 = scanner.nextInt();
+        System.out.printf("%d is %s",num3, (num3 % 2 == 0 ? "even" : "odd"));
+
+        System.out.println("Enter red or green");
+        String color = scanner.nextLine();
+        if(color.toLowerCase() == "red"){
+            System.out.println("stop");
+        } else{
+            System.out.println("go");
+        }
+        */
+
+    }
+}
